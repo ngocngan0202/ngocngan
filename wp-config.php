699c89cce4efe3e1b026fb48db0ac2cfe0056c97
ngocngan0202 <ngocngan1711@gmail.com>
2017-06-05 18:07:49 +0700

    Update_DB

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'ngocngan');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ngocngan');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Ng@n_1711');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
